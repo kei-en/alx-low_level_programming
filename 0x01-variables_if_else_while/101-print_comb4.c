1d2a3cc544165ec4350939d21dbe7731ff427930
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 <njuguna0karanja@gmail.com>
2023-02-18 11:33:38 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int n1, n2, n3;
 
-	for (n1 = 0; n1 <= 8; n1++)
+	for (n1 = 0; n1 < 8; n1++)
 	{
-		for (n2 = n1 + 1; n2 <= 9; n2++)
+		for (n2 = n1 + 1; n2 < 9; n2++)
 		{
-			for (n3 = n2 + 1; n3 <= 10; n3++)
+			for (n3 = n2 + 1; n3 < 10; n3++)
 			{
 				putchar((n1 % 10) + '0');
 				putchar((n2 % 10) + '0');
