cb9e5e6cf5281ef97bb8654cea97306cabc90995
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 <njuguna0karanja@gmail.com>
2023-02-23 05:38:53 -0800

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: void
+*/
+void more_numbers(void)
+{
+	int r, n;
+
+	for (r = 0; r <= 10; r++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				_putchar((n / 10) + '0');
+
+			_putchar((n % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@ Contains:
 1-isdigit.c - checks fro a digit (0 through 9)
 2-mul.c - multiplies two integers
 3-print_numbers.c - prints the numbers from 0 to 9
+5-more-numbers.c - prints 10 times the numbers from 0 to 14
