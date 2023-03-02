951250500b2d9475d381ced0e917c4d1d058ef4d
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 <njuguna0karanja@gmail.com>
2023-03-02 01:28:17 -0800

    strcat.c revision

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	for (length = 0; dest[length] != '\0'; length++)
 		;
 
-	for (i = 0; dest[i] != '\0'; ++i, ++length)
+	for (i = 0; src[i] != '\0'; ++i, ++length)
 		dest[length] = src[i];
 
 	dest[length] = '\0';
