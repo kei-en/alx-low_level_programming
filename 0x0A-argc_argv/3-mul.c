8a79da54c1abf3da488a15d463239abb4301c5ff
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 <njuguna0karanja@gmail.com>
2023-03-11 05:58:52 -0800

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+*/
+int main(int argc, char *argv[])
+{
+	int mul, i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	mul = i * j;
+
+	printf("%d\n", mul);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,3 +2,4 @@ Contains:
 0-whatsmyname.c - prints the name of this program
 1-args.c - prints the number of arguments passed into it
 2-args.c - prints all arguments it receives
+3-mul.c - multiplies two numbers
