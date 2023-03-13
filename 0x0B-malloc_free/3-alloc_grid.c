9f7f14ee169e5b6e0731a124f46b54bb1dc74ec1
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 <njuguna0karanja@gmail.com>
2023-03-13 06:00:58 -0700

    If you dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: first array
+ * @height: second array
+ *
+ * Return: pointer to the 2 dimensional array (success), NULL (fail)
+*/
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	array = malloc(sizeof(int) * width);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		array[i] = malloc(sizeof(int) * height);
+		if (array[i] == NULL)
+		{
+			for ( ; i >= 0; i--)
+				free(array[i]);
+
+			free(array);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+
+	return (array);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -2,3 +2,4 @@ Contains:
 0-create_array.c - creates an array of chars, and initializes it with a specific char
 1-strdup.c - returna a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter
 2-str_concat.c - concatenates two strings
+3-alloc-grid.c - returns a pointer to a 2 dimensional array of integers
