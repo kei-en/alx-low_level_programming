2e5e11d91ba31b2c7d1eff498d66adf88100aad3
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 <njuguna0karanja@gmail.com>
2023-03-13 05:01:01 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: pointer to concatenated string (success), NULL (fail)
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int s1_len, s2_len, i;
+	int size = 0;
+	char *s;
+
+	for (s1_len = 0; s1[s1_len]; s1_len++)
+		;
+	for (s2_len = 0; s2[s2_len]; s2_len++)
+		;
+
+	size = s1_len + s2_len + 1;
+
+	s = malloc(sizeof(char) * size);
+
+	if (s == NULL)
+	{
+		s[size - 1] = '\0';
+		return (NULL);
+	}
+
+	for (i = 0; i < s1_len; i++)
+		s[i] = s1[i];
+
+	for (i = 0; i < s2_len; i++)
+		s[s1_len + i] = s2[i];
+
+	s[size - 1] = '\0';
+
+	return (s);
+}
