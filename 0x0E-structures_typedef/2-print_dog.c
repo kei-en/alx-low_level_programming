81156df3df8e706d6b2df68f619accfa6afc89d5
kei-en <njuguna0karanja@gmail.com>
2023-03-21 03:59:09 -0700

    A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: a type of dog
+ *
+ * Return: void (success)
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == 0.0f)
+			printf("Age: (nil)");
+		else
+			printf("Age: %.6f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,2 +1,3 @@
 Contains:<br />**dog.h** - defines a new type struct dog<br />
 **1-init_dog.c** - initializes a varibale of type struct dog
+<br />**2-print_dog.c** - prints a struct dog
