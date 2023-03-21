1cbdbf6f54def677814d5b87eaf17e18607c8f6b
kei-en <njuguna0karanja@gmail.com>
2023-03-21 03:22:03 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Contains:<br />**dog.h** - defines a new type struct dog<br />
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - new type defining a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
