63ebd9e4419cdea440bb4532d8e49a3635f7f5e7
kei-en <njuguna0karanja@gmail.com>
2023-04-03 07:07:22 -0700

    Add node at the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint-t list
+ * @head: a pointer to the address of the head of the list (listint_t)
+ * @n: integer of the new node
+ *
+ * Return: address of the new element (success), NULL (fail)
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newNode, *lastNode;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+		*head = newNode;
+
+	while (lastNode->next != NULL)
+		lastNode = lastNode->next;
+
+	lastNode->next = newNode;
+	return (newNode);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,3 +1,4 @@
 Contains:<br />**0-print_listint.c** - prints all the elements of a *listint_t* list
 <br />**1-listint_len.c** - returns the number of elements in a linked *listint_len* list
 <br />**2-add_nodeint.c** - adds a new node at the beginning of a *listint-t* list
+<br />**3-add_nodeint_end.c** - adds a new node at the end of a *listint_t* list
